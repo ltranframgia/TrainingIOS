454e5c563fe12e6ee16f21c12947f2dd88cdac66
mod bibi <admin@admins-MacBook-Air.local>
2017-07-18 09:04:54 +0700

    Initial Commit

---
+++ TrainingIOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TrainingIOS
+//
+//  Created by mod bibi on 7/18/17.
+//  Copyright © 2017 DaoNhat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
